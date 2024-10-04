550932bfdd5b386054049d84b8d538c3745073ed
joaoburi <joaoburi64@gmail.com>
2024-10-04 01:16:42 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   state = {
-    name: 'Otávio Miranda',
-    counter: 0
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      }
+    ]
   };
-  handlePClick = () => {
-    this.setState({ name: 'Buri' });
+  
+  timeoutUpdate = null; 
+
+  componentDidMount() {
+    this.handleTimeOut();
   }
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+
+  componentDidUpdate() {
+    this.handleTimeOut();
   }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O titulo mudou';
+
+    
+   this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 3000);
+  }
+   
 
   render() {
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
+ 
+
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+          ))}
       </div>
     );
   }
 }
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//            <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
