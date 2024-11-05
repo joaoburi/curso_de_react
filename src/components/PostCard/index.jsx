d54fe41b2a7cc1c4f982f3b66e725f93c6e8452b
joaoburi <joaoburi64@gmail.com>
2024-11-05 18:11:15 -0300

    att conteudo

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,13 +1,11 @@
-export const PostCard = ({title, cover, body, id}) => {
-  
+import './styles.css';
 
-  
+export const PostCard = ({ title, cover, body, id }) => (
   <div className="post">
-    <img src={cover} alt={title}/>
+    <img src={cover} alt={title} />
     <div className="post-content">
-      <h1> {title} </h1>
-      <p> {body} </p>
+      <h2>{title} {id}</h2>
+      <p>{body}</p>
     </div>
   </div>
-
-}
+  );
